6079d9dd61bb5cd5e213359907e5355408ae7563
luxp <luxianpo@gmail.com>
2023-02-16 09:38:10 +0800

    换一种qiniu 的写法，兼容本地

--- api/qiniu-upload-token.ts
+++ api/qiniu-upload-token.ts
@@ -1,18 +1,16 @@
-import qiniu from 'qiniu'
+import { auth as qiniuAuth, rs as qiniuRs } from 'qiniu'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 var accessKey = 'v8KS71ioKp7tLzb7rzXdYofso5CkNO9rIwfE139M'
 var secretKey = 'E3Z-ljUYJtETEkd4KSpEev3IfurdDpPOCwQgdEUu'
 
-console.log(qiniu)
-
-const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+const mac = new qiniuAuth.digest.Mac(accessKey, secretKey)
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const options = {
     scope: 'luxianpo',
   }
-  const putPolicy = new qiniu.rs.PutPolicy(options)
+  const putPolicy = new qiniuRs.PutPolicy(options)
   const uploadToken = putPolicy.uploadToken(mac)
 
   res.status(200).json({ uploadToken })
