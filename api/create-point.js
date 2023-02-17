8f53041817fc9fd3280231649afe525993b8ceff
luxp <luxianpo@gmail.com>
2023-02-17 23:52:05 +0800

    add api 就直接用 esm 的方法写

--- api/create-point.js
+++ api/create-point.js
@@ -1,35 +1,36 @@
-import { MongoClient, Db } from 'mongodb'
-
-let uri =
+// src/api/utils/mongodb.ts
+import { MongoClient } from 'mongodb'
+var uri =
   process.env.MONGODB_URI || 'mongodb://admin:mylu123654@127.0.0.1:27017'
-let dbName = process.env.MONGODB_DB || 'localtest'
-
-let cachedClient: MongoClient
-let cachedDb: Db
-
+var dbName = process.env.MONGODB_DB || 'localtest'
+var cachedClient
+var cachedDb
 if (!uri) {
   throw new Error(
     'Please define the MONGODB_URI environment variable inside .env.local'
   )
 }
-
 if (!dbName) {
   throw new Error(
     'Please define the MONGODB_DB environment variable inside .env.local'
   )
 }
-
-export async function connectToDatabase() {
+async function connectToDatabase() {
   if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb }
   }
-
   const client = await MongoClient.connect(uri)
-
   const db = await client.db(dbName)
-
   cachedClient = client
   cachedDb = db
-
   return { client, db }
+}
+
+export default async function handler(req, res) {
+  const { body } = req
+  const { db } = await connectToDatabase()
+  const pointsCollection = db.collection('points')
+  pointsCollection.insertOne(body)
+  console.log(body)
+  res.status(200).json({})
 }
--- api/qiniu-upload-token.js
+++ api/qiniu-upload-token.js
@@ -1,18 +1,15 @@
 import qiniu from 'qiniu'
-import type { VercelRequest, VercelResponse } from '@vercel/node'
 
-// TODO: 后面这个直接换成 js 的好了
 var accessKey = 'v8KS71ioKp7tLzb7rzXdYofso5CkNO9rIwfE139M'
 var secretKey = 'E3Z-ljUYJtETEkd4KSpEev3IfurdDpPOCwQgdEUu'
 
-const mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
+var mac = new qiniu.auth.digest.Mac(accessKey, secretKey)
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req, res) {
   const options = {
     scope: 'luxianpo',
   }
   const putPolicy = new qiniu.rs.PutPolicy(options)
   const uploadToken = putPolicy.uploadToken(mac)
-
   res.status(200).json({ uploadToken, t: 'dd12312' })
 }
